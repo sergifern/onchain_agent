c0dc9c09eb7449ace970fe9964cb5b2dee3f8fd1
Sergi <sergi.fernandez.fort@gmail.com>
2025-02-26 11:49:02 +0100

    fix og

--- app/(dashboard)/namespace/[domain]/[...slug]/page.tsx
+++ app/(dashboard)/namespace/[domain]/[...slug]/page.tsx
@@ -3,7 +3,7 @@ import { Metadata } from "next"
 import DocumentPage from "./DocPage"
 
 
-const metadata: Metadata = {
+export const metadata: Metadata = {
   title: `Ethy AI - Documentation`,
   description: 'Ethy AI Description',
   openGraph: {
