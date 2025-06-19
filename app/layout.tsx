82924fe50c7db1bfeacbe34adf6c94be5891590d
Sergi <sergi.fernandez.fort@gmail.com>
2025-06-19 09:50:27 +0200

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,6 +60,15 @@ export default function RootLayout({
           gtag('config', 'G-CW15Z8E7K5');
         `}
         </Script>
+
+        <Script id="twitter-pixel">
+          {`
+            !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+            },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+            a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+            twq('config','p0d9v');
+          `}
+        </Script>
       </head>
       <body className={`${inter.className} ${funnelDisplay.variable} ${hansengrotesk.variable}`}>
         <Providers>
